4fc577307b7f81a394afc8c03c6b2a59a761998e
ithikornn <ithikorn.13853@gmail.com>
2024-10-03 13:11:02 +0700

    link payment button from order deatail page to invoice page

--- app/orders/[...o_id]/page.tsx
+++ app/orders/[...o_id]/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/app/components/Navbar";
 import Image from "next/image";
+import Link from "next/link";
 import { IoIosArrowForward } from "react-icons/io";
 
 export default function OrderDetail() {
@@ -50,9 +51,11 @@ export default function OrderDetail() {
                             </button>
                         </div>
                         <div>
+                            <Link href={`/invoice`}>
                             <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                 ชำระเงิน
                             </button>
+                            </Link>
                         </div>
                     </div>
                 </div>
